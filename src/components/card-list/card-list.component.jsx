c81edba37344117c3d2a48aeadd91a16deab438d
malayvasa <malayvasa@gmail.com>
2020-11-17 21:52:18 +0530

    Animation v1.0

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import {Card} from '../card/card.component'
 import './card-list.styles.css'
+import {motion, AnimatePresence} from 'framer-motion'
 
 export const CardList = props => {
-    return(<div className="card-list">{
+    return(
+    <AnimatePresence initial={true}>
+    <div 
+    className="card-list"> {
         props.characters.map(character => (
-        <Card key={character.id} character={character}/>
-     ))}</div>);
+        <motion.li
+        variants ={{
+            hidden: {
+                opacity :0,
+            },
+            visible: (i) => ({
+                opacity:1,
+                transition :{
+                    delay: 0.125*i,
+                }
+            })
+        }}
+        animate= "visible"
+        initial= "hidden"
+        custom={character.id}
+        className="card-li" key={character.id}>
+            <Card character={character}/>
+        </motion.li>
+     ))}
+     </div>
+     </AnimatePresence>
+     );
  };
