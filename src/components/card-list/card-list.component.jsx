5ceb08b439aadd5f26ca38ea1150ffb3926e5f77
malayvasa <malayvasa@gmail.com>
2020-11-17 22:21:13 +0530

    Remove Animation

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -4,31 +4,12 @@ import './card-list.styles.css'
 import {motion, AnimatePresence} from 'framer-motion'
 
 export const CardList = props => {
+    // let x = props.characters[0].id;
     return(
-    <AnimatePresence initial={true}>
-    <div 
-    className="card-list"> {
-        props.characters.map(character => (
-        <motion.li
-        variants ={{
-            hidden: {
-                opacity :0,
-            },
-            visible: (i) => ({
-                opacity:1,
-                transition :{
-                    delay: 0.125*i,
-                }
-            })
-        }}
-        animate= "visible"
-        initial= "hidden"
-        custom={character.id}
-        className="card-li" key={character.id}>
-            <Card character={character}/>
-        </motion.li>
+    <div className="card-list"> 
+    {props.characters.map((character,i) => (
+            <Card key={character.id} character={character}/>
      ))}
      </div>
-     </AnimatePresence>
      );
  };
