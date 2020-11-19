c505bfbf918e862d9467ee08bbe276c23cb10b3d
malayvasa <malayvasa@gmail.com>
2020-11-19 16:22:19 +0530

    Safe Commit

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import {Card} from '../card/card.component'
 import './card-list.styles.css'
-import {motion, AnimatePresence} from 'framer-motion'
 
 export const CardList = props => {
-    // let x = props.characters[0].id;
     return(
     <div className="card-list"> 
     {props.characters.map((character,i) => (
